c535d5e89c17f2a2aecfbd3e9bfccfc094b6d7c3
Aleksey Salnikov <me@iamsalnikov.ru>
2020-03-08 08:51:18 +0300

    Applied func test

--- migration_test.go
+++ migration_test.go
@@ -104,6 +104,142 @@ func Test_NewMigrationNames(t *testing.T) {
 	}
 }
 
+func Test_MigratorApply(t *testing.T) {
+	applyErr := errors.New("apply err")
+	markAppliedErr := errors.New("mark applied err")
+	err1 := errors.New("err1")
+	err2 := errors.New("err2")
+
+	type testCase struct {
+		migrations       map[string]UpFunc
+		applyErr         error
+		markAppliedErr   error
+		expectedErr      error
+		expectMarkedCall map[string]bool
+	}
+
+	testCases := []testCase{
+		{
+			migrations:       map[string]UpFunc{},
+			applyErr:         nil,
+			markAppliedErr:   nil,
+			expectedErr:      nil,
+			expectMarkedCall: map[string]bool{},
+		},
+		{
+			migrations:       map[string]UpFunc{},
+			applyErr:         applyErr,
+			markAppliedErr:   nil,
+			expectedErr:      applyErr,
+			expectMarkedCall: map[string]bool{},
+		},
+		{
+			migrations:       map[string]UpFunc{},
+			applyErr:         nil,
+			markAppliedErr:   markAppliedErr,
+			expectedErr:      nil,
+			expectMarkedCall: map[string]bool{},
+		},
+		{
+			migrations: map[string]UpFunc{
+				"0": func(db *bbolt.DB) error { return nil },
+			},
+			applyErr:         applyErr,
+			markAppliedErr:   markAppliedErr,
+			expectedErr:      applyErr,
+			expectMarkedCall: map[string]bool{},
+		},
+		{
+			migrations: map[string]UpFunc{
+				"0": func(db *bbolt.DB) error { return nil },
+			},
+			applyErr:         nil,
+			markAppliedErr:   markAppliedErr,
+			expectedErr:      markAppliedErr,
+			expectMarkedCall: map[string]bool{"0": true},
+		},
+		{
+			migrations: map[string]UpFunc{
+				"0": func(db *bbolt.DB) error { return err1 },
+				"1": func(db *bbolt.DB) error { return err2 },
+			},
+			applyErr:         nil,
+			markAppliedErr:   nil,
+			expectedErr:      err1,
+			expectMarkedCall: map[string]bool{},
+		},
+		{
+			migrations: map[string]UpFunc{
+				"0": func(db *bbolt.DB) error { return nil },
+				"1": func(db *bbolt.DB) error { return err2 },
+			},
+			applyErr:         nil,
+			markAppliedErr:   nil,
+			expectedErr:      err2,
+			expectMarkedCall: map[string]bool{"0": true},
+		},
+		{
+			migrations: map[string]UpFunc{
+				"0": func(db *bbolt.DB) error { return err1 },
+				"1": func(db *bbolt.DB) error { return nil },
+			},
+			applyErr:         nil,
+			markAppliedErr:   nil,
+			expectedErr:      err1,
+			expectMarkedCall: map[string]bool{},
+		},
+		{
+			migrations: map[string]UpFunc{
+				"0": func(db *bbolt.DB) error { return nil },
+				"1": func(db *bbolt.DB) error { return nil },
+			},
+			applyErr:         nil,
+			markAppliedErr:   nil,
+			expectedErr:      nil,
+			expectMarkedCall: map[string]bool{"0": true, "1": true},
+		},
+	}
+
+	for i, c := range testCases {
+		resetMigrations()
+		resetAppliedFunc()
+		resetMarkAppliedFunc()
+
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			// we've already tested NewNames() function
+			// so, here we will return always empty slice
+			getApplied = AppliedFunc(func(db *bbolt.DB) ([]string, error) {
+				return []string{}, c.applyErr
+			})
+
+			markedCall := make(map[string]bool)
+			markApplied = MarkAppliedFunc(func(db *bbolt.DB, name string) error {
+				if !c.expectMarkedCall[name] {
+					t.Errorf("I didn't excpect that mig '%s' will be marked as aplied", name)
+				}
+
+				markedCall[name] = true
+
+				return c.markAppliedErr
+			})
+
+			for name, mig := range c.migrations {
+				Add(name, mig)
+			}
+
+			err := Apply()
+			if err != c.expectedErr {
+				t.Errorf("I expected to get error \"%v\" but got \"%v\"", c.expectedErr, err)
+			}
+
+			if len(markedCall) != len(c.expectMarkedCall) {
+				t.Errorf("I expected that these migrations (%+v) we will try to mark as applied. "+
+					"But we tried to mark as applied %+v", c.expectMarkedCall, markedCall)
+			}
+		})
+	}
+}
+
 func isEqualSlices(a, b []string) bool {
 	if len(a) != len(b) {
 		return false
